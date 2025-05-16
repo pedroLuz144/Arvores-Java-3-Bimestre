bae385cf7afdc355cac455653336112cb0248fdd
Pedro Luz <p.h.r.s.l123@gmail.com>
2025-05-15 21:55:39 -0300

    Atividade para implementar em Java, uma classe Nó e uma classe Arvore

---
+++ 15_05_2025/src/No.java
@@ -0,0 +1,9 @@
+public class No {
+    String valor;
+    No filhoEsquerdo, filhoDireito;
+
+    public No(String valor) {
+        this.valor = valor;
+        filhoEsquerdo = filhoDireito = null;
+    }
+}   
