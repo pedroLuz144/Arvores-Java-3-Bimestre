fe0a8242ef0dc2f944040bd97b65b731601f2f27
Pedro Luz <p.h.r.s.l123@gmail.com>
2025-05-22 21:59:20 -0300

    emOrder, posOrder e emOrder

--- 15_05_2025/src/App.java
+++ 15_05_2025/src/App.java
@@ -2,13 +2,28 @@ public class App {
     public static void main(String[] args) throws Exception {
         Arvore arvore = new Arvore();
         arvore.raiz = new No("A");
+
         arvore.raiz.filhoEsquerdo = new No("B");
+        arvore.raiz.filhoEsquerdo.filhoEsquerdo = new No("D");
+        arvore.raiz.filhoEsquerdo.filhoDireito = new No("E");
+        arvore.raiz.filhoEsquerdo.filhoDireito.filhoEsquerdo = new No("H");
+
         arvore.raiz.filhoDireito = new No("C");
+        arvore.raiz.filhoDireito.filhoEsquerdo = new No("F");
+        arvore.raiz.filhoDireito.filhoDireito = new No("G");
         // arvore.setRaiz(raiz);
         // raiz.setFilhoEsquerdo(new No("1"));
         // raiz.setFilhoDireito(new No("2"));
 
+        System.out.println();
         arvore.exibirNumeroDeNos();
+        System.out.println();
         arvore.preOrder(arvore.raiz);
+        System.out.println("\n");
+        arvore.emOrder(arvore.raiz);
+        System.out.println("\n");
+        arvore.posOrder(arvore.raiz);
+        System.out.println("\n");
+        arvore.emNivel(arvore.raiz);
     }
 }
--- 15_05_2025/src/Arvore.java
+++ 15_05_2025/src/Arvore.java
@@ -1,6 +1,13 @@
 public class Arvore {
         No raiz;
 
+        public Arvore() {
+        }
+
+        public Arvore(No raiz) {
+                this.raiz = raiz;
+        }
+
         public No getRaiz() {
                 return raiz;
         }
@@ -18,11 +25,11 @@ public class Arvore {
         }
 
         public void exibirNumeroDeNos() {
-        int totalDeNos = contarNos(raiz);
-        System.out.println("Essa arvore tem " + totalDeNos + " nós."); 
+                int totalDeNos = contarNos(raiz);
+                System.out.println("Essa arvore tem " + totalDeNos + " nós.");
         }
 
-        //método para percorrer os nós da árvore em pré-ordem.
+        // método para percorrer os nós da árvore em pré-ordem.
         public void preOrder(No raiz) {
                 if (raiz != null) {
                         System.out.print(raiz.valor + ", ");
@@ -31,16 +38,59 @@ public class Arvore {
                 }
         }
         // public void preOrder(No arvore) {
-        //         if (arvore == null) {
-        //                 System.out.println("Nó vazio"); 
-        //         } else {
-        //                 System.out.println(arvore);
-        //         }
-                
-        //         if (arvore.raiz.filhoEsquerdo == null) {
-        //                 System.out.println("Sem filho esquerdo... Filho direito da raiz = " + raiz + " = " + raiz.filhoEsquerdo);
-        //         } else {
-        //                 System.out.println("Filho esquerdo da raiz = " + raiz + " = " + raiz.filhoEsquerdo);
-        //         }
+        // if (arvore == null) {
+        // System.out.println("Nó vazio");
+        // } else {
+        // System.out.println(arvore);
         // }
+
+        // if (arvore.raiz.filhoEsquerdo == null) {
+        // System.out.println("Sem filho esquerdo... Filho direito da raiz = " + raiz +
+        // " = " + raiz.filhoEsquerdo);
+        // } else {
+        // System.out.println("Filho esquerdo da raiz = " + raiz + " = " +
+        // raiz.filhoEsquerdo);
+        // }
+        // }
+
+        public void emOrder(No raiz) {
+                if (raiz != null) {
+                        emOrder(raiz.filhoEsquerdo);
+                        System.out.print(raiz.valor + ", ");
+                        emOrder(raiz.filhoDireito);
+                }
+        }
+
+        public void posOrder(No raiz) {
+                if (raiz != null) {
+                        posOrder(raiz.filhoEsquerdo);
+                        posOrder(raiz.filhoDireito);
+                        System.out.print(raiz.valor + ", ");
+                }
+        }
+
+        public void emNivel(No raiz) {
+                int lado = 1;
+                No raiz2;
+                System.out.print(raiz.valor + ", ");
+                do {
+                        if (raiz != null) {
+                                if (lado == 1) {
+                                        System.out.print(raiz.filhoEsquerdo.valor + ", ");
+                                        System.out.print(raiz.filhoDireito.valor + ", ");
+                                        raiz2 = raiz.filhoEsquerdo;
+                                        lado = 2;
+                                        if (lado == 2) {
+                                        System.out.print(raiz2.filhoEsquerdo.valor + ", ");
+                                        System.out.print(raiz2.filhoDireito.valor + ", ");
+                                        raiz = raiz.filhoDireito;
+                                        lado = 1;
+                                        }
+                                }
+                        } else {
+                                lado = 0;
+                        }
+
+                } while (lado != 0);
+        }
 }
